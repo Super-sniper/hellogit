f6c84dd3d25a95ba2b57b459b49318203a8f3763
Super-sniper <31527034+Super-sniper@users.noreply.github.com>
2018-03-25 15:57:46 +0800

    Update HelloGit.java

--- src/hellogit/HelloGit.java
+++ src/hellogit/HelloGit.java
@@ -3,7 +3,7 @@ package hellogit;
 public class HelloGit {
 	public static void main(String[] args) {
 		
-		sysem.out.println("129到此一游");
+		system.out.println("129到此一游");
 		System.out.println("Hello Git abc ");
 	}
 }
